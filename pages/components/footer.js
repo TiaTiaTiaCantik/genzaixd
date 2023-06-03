7c2aff3fa153c741a0a60739192f87b84384c970
TiaTiaTiaCantik <134162252+TiaTiaTiaCantik@users.noreply.github.com>
2023-06-04 00:48:25 +0700

    Add files via upload

---
+++ pages/components/footer.js
@@ -0,0 +1,17 @@
+import {BsInstagram, BsTiktok, BsTelegram} from "react-icons/bs";
+import { useRouter } from 'next/router'
+export default function footer() {
+    const router = useRouter()
+  return (
+    <>
+      <footer>
+        <p>Follow me on Social Medias: </p>
+        <div className="icons">
+        <BsInstagram className="button" onClick={() => router.push('https://www.instagram.com/genzai666')}/>
+        <BsTiktok className="button" onClick={() => router.push('https://vt.tiktok.com/Available6664')}/>
+        <BsTelegram className="button" onClick={() => router.push('https://t.me/genzai666')}/>
+        </div>
+      </footer>
+    </>
+  )
+}
