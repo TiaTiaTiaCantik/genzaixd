aff229b725a68b7b965b02338ee38fda20d92307
TiaTiaTiaCantik <134162252+TiaTiaTiaCantik@users.noreply.github.com>
2023-06-18 14:09:37 +0700

    Update Projects.jsx

--- pages/components/Projects.jsx
+++ pages/components/Projects.jsx
@@ -5,22 +5,22 @@ export default function Projects() {
     <>
       <div className="projectscontainer">
         <div className="projectborder">
-          <img src="https://telegra.ph/file/db373f7cf52513c97fe76.jpg" alt="Social Media" />
-        <Link href="https://teer.id/genzai" target="_blank">
+          <img src="" alt="https://telegra.ph/file/6105567ccd68513eb8ce5.jpg" />
+        <Link href="" target="_blank">
         <CgMoreO className="visiticon"/>
         </Link>
         </div>
         <div className="projectborder">
-        <img src="https://telegra.ph/file/6105567ccd68513eb8ce5.jpg" alt="" />
-        <h1>Error-page</h1>
-        <Link href="https://404-not-found" target="_blank">
+        <img src="https://telegra.ph/file/db373f7cf52513c97fe76.jpg" alt="" />
+        <h1>Social Media</h1>
+        <Link href="https://teer.id/genzai" target="_blank">
         <CgMoreO className="visiticon"/>
         </Link>
         </div>
         <div className="projectborder">
         <img src="https://telegra.ph/file/6105567ccd68513eb8ce5.jpg"  />
         <h1>Error-page</h1>
-        <Link href="https://404-not-found" target="_blank">
+        <Link href="" target="_blank">
         <CgMoreO className="visiticon"/>
         </Link>
         </div>
