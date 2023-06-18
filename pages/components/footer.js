fd92c3c662dfdba2793b233e0ffde4879a3726f7
TiaTiaTiaCantik <134162252+TiaTiaTiaCantik@users.noreply.github.com>
2023-06-18 12:55:32 +0700

    Update footer.js

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -7,7 +7,7 @@ export default function footer() {
       <footer>
         <p>Follow me on Social Medias: </p>
         <div className="icons">
-        <BsInstagram className="button" onClick={() => router.push('https://www.instagram.com/genzai666')}/>
+        <BsInstagram className="button" onClick={() => router.push('https://www.instagram.com/genzai66')}/>
         <BsTiktok className="button" onClick={() => router.push('https://vt.tiktok.com/Available6664')}/>
         <BsTelegram className="button" onClick={() => router.push('https://t.me/genzai666')}/>
         <BsWhatsApp className="button" onClick={() => router.push('https://wa.me/message/MILF7ER53RI2A1')}/>
