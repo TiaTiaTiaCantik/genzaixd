38a15738832b34c297cbee818598d05b4fc252fa
TiaTiaTiaCantik <134162252+TiaTiaTiaCantik@users.noreply.github.com>
2023-06-18 13:50:18 +0700

    Update Projects.jsx

--- pages/components/Projects.jsx
+++ pages/components/Projects.jsx
@@ -5,8 +5,8 @@ export default function Projects() {
     <>
       <div className="projectscontainer">
         <div className="projectborder">
-          <img src="https://telegra.ph/file/6105567ccd68513eb8ce5.jpg" alt="" />
-        <Link href="https://404-not-found" target="_blank">
+          <img src="https://telegra.ph/file/6105567ccd68513eb8ce5.jpg" alt="Social Media" />
+        <Link href="https://teer.id/genzai" target="_blank">
         <CgMoreO className="visiticon"/>
         </Link>
         </div>
